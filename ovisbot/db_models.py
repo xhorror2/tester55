b357a45164e8c45b4fa5bc0c18d870e3369f23c0
Glitch (apogiatzis-kyrioszolo) <none>
2020-04-19 01:33:55 +0000

    👑🏛 Updated with Glitch

---
+++ ovisbot/db_models.py
@@ -0,0 +1,108 @@
+import os
+import logging
+
+from ovisbot.helpers import escape_md
+import ovisbot.locale as i118n
+from pymodm import MongoModel, EmbeddedMongoModel, fields, connect
+from ovisbot.utils.progressbar import draw_bar
+
+logger = logging.getLogger(__name__)
+connect("mongodb://mongo/ovisdb")
+
+
+class BotConfig(MongoModel):
+    ANNOUNCEMENTS_CHANNEL = fields.IntegerField()
+    REMINDERS_CHANNEL = fields.IntegerField()
+    IS_MAINTENANCE = fields.BooleanField()
+
+
+class InstalledCogs(MongoModel):
+    name = fields.CharField(required=True)
+    enabled = fields.BooleanField(default=True)
+
+
+class HTBUserMapping(MongoModel):
+    discord_user_id = fields.IntegerField(required=True)
+    htb_user = fields.CharField(required=True)
+    htb_user_id = fields.IntegerField(required=True)
+
+
+class CryptoHackUserMapping(MongoModel):
+    discord_user_id = fields.IntegerField(required=True)
+    cryptohack_user = fields.CharField(required=True)
+
+
+class Challenge(EmbeddedMongoModel):
+    name = fields.CharField(required=True)
+    created_at = fields.DateTimeField(required=True)
+    tags = fields.ListField(fields.CharField(), default=[])
+    attempted_by = fields.ListField(fields.CharField(), default=[])
+    solved_at = fields.DateTimeField(blank=True)
+    solved_by = fields.ListField(fields.CharField(), default=[], blank=True)
+    notebook_url = fields.CharField(default="", blank=True)
+    flag = fields.CharField()
+
+
+class CTF(MongoModel):
+    name = fields.CharField(required=True)
+    description = fields.CharField()
+    created_at = fields.DateTimeField(required=True)
+    finished_at = fields.DateTimeField()
+    start_date = fields.DateTimeField()
+    end_date = fields.DateTimeField()
+    url = fields.URLField()
+    username = fields.CharField()
+    password = fields.CharField()
+    challenges = fields.EmbeddedDocumentListField(Challenge, default=[], blank=True)
+    pending_reminders = fields.ListField(blank=True, default=[])
+
+    def status(self, members_joined_count):
+
+        description_str = self.description + "\n" if self.description else ""
+
+        solved_count = len(
+            list(filter(lambda x: x.solved_at is not None, self.challenges))
+        )
+        total_count = len(self.challenges)
+        status = (
+            f":triangular_flag_on_post: **{self.name}** ({members_joined_count} Members joined)\n{description_str}"
+            + f"```CSS\n{draw_bar(solved_count, total_count, style=5)}\n"
+            + f" {solved_count} Solved / {total_count} Total"
+        )
+        if self.start_date:
+            fmt_str = "%d/%m %H:\u200b%M"
+            start_date_str = self.start_date.strftime(fmt_str)
+            end_date_str = self.end_date.strftime(fmt_str) if self.end_date else "?"
+            status += f"\n {start_date_str} - {end_date_str}\n"
+        status += "```"
+        return status
+
+    def credentials(self):
+        response = f":busts_in_silhouette: **Username**: {self.username}\n:key: **Password**: {self.password}"
+        if self.url is not None:
+            response += f"\n\nLogin Here: {self.url}"
+        return response
+
+    def challenge_summary(self):
+        if not self.challenges:
+            return i118n._(
+                "No challenges found. Try adding one with `!ctf addchallenge <name> <category>`"
+            )
+
+        solved_response, unsolved_response = "", ""
+
+        for challenge in self.challenges:
+            challenge_details = f'**{escape_md(challenge.name[len(self.name)+1:])}** [{", ".join(challenge.tags)}]'
+            if challenge.solved_at:
+                solved_response += f':white_check_mark: {challenge_details} Solved by: [{", ".join(challenge.solved_by)}]\n'
+            else:
+                unsolved_response += f':thinking: {challenge_details} Attempted by: [{escape_md(", ".join(challenge.attempted_by))}]\n'
+
+        summary = (
+            f"\\>>> Solved\n{solved_response}" + f"\\>>> Unsolved\n{unsolved_response}"
+        )
+        return summary
+
+    class Meta:
+        collection_name = "ctf"
+        ignore_unknown_fields = True
---
+++ ovisbot/helpers.py
@@ -0,0 +1,59 @@
+import requests
+import os
+import urllib.parse
+from discord.ext.commands.core import GroupMixin
+
+
+def chunkify(text, limit):
+    chunks = []
+    while len(text) > limit:
+        idx = text.index("\n", limit)
+        chunks.append(text[:idx])
+        text = text[idx:]
+    chunks.append(text)
+    return chunks
+
+
+def escape_md(text):
+    return text.replace("_", "\_").replace("*", "\*").replace(">>>", "\>>>")
+
+
+def create_corimd_notebook():
+    base_url = "https://notes.status.im/"
+    create_new_note_url = base_url + "new"
+    res = requests.get(create_new_note_url)
+    return res.url
+
+
+def wolfram_simple_query(query):
+    base_url = "https://api.wolframalpha.com/v2/result?i={0}&appid={1}"
+    app_id = os.getenv("WOLFRAM_ALPHA_APP_ID")
+    query_url = base_url.format(urllib.parse.quote(query), app_id)
+    res = requests.get(query_url).text
+    return res
+
+
+def td_format(td_object):
+    ## Straight copy paste from here: https://stackoverflow.com/questions/538666/format-timedelta-to-string
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ("year", 60 * 60 * 24 * 365),
+        ("month", 60 * 60 * 24 * 30),
+        ("day", 60 * 60 * 24),
+        ("hour", 60 * 60),
+        ("minute", 60),
+        ("second", 1),
+    ]
+
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds > period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            has_s = "s" if period_value > 1 else ""
+            strings.append("%s %s%s" % (period_value, period_name, has_s))
+
+    return ", ".join(strings)
+
+
+async def success(message):
+    await message.add_reaction("✅")
